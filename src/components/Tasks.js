f5310ddc9f0e9b4a155b98787c8a3da8584ccaea
Paula Minteguiaga <paulaminteguiaga@gmail.com>
2021-05-10 23:08:03 -0300

    update

---
+++ src/components/Tasks.js
@@ -0,0 +1,14 @@
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete, onToggleReminder }) => {
+    
+    return (
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete} onToggleReminder={onToggleReminder} />
+            ))}
+        </>
+    )
+}
+
+export default Tasks
